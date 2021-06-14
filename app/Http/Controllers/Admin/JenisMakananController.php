ca544e855bb6c13d07270ef72c304dc37783d2f4
fadil2227 <kirifa2710@gmail.com>
2021-06-14 20:59:20 +0700

    Pull terooss

---
+++ app/Http/Controllers/Admin/JenisMakananController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\JenisMakanan;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use RealRashid\SweetAlert\Facades\Alert;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Paginator;
+use Illuminate\Http\Request;
+
+class JenisMakananController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $JenisMakanan = JenisMakanan::all();
+
+        return view('admin.JenisMakanan.index', compact('JenisMakanan'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.JenisMakanan.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Alert::success('Success', 'Berhasil menambah data kamar');
+
+        JenisMakanan::create([
+            'JenisMakanan' => request('JenisMakanan')
+        ]);
+
+        return redirect()->route('admin.JenisMakanan.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(JenisMakanan $JenisMakanan)
+    {
+        return view('admin.JenisMakanan.edit', compact('JenisMakanan'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, JenisMakanan $JenisMakanan)
+    {
+        Alert::success('Success', 'Berhasil mengedit data JenisMakanan');
+
+        $JenisMakanan->update([
+
+            'JenisMakanan' => request('JenisMakanan')
+
+        ]);
+
+        return redirect()->route('admin.JenisMakanan.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(JenisMakanan $JenisMakanan)
+    {
+        $JenisMakanan->delete();
+        Alert::success('Success', 'Berhasil menghapus data menu JenisMakanan');
+
+        return redirect()->route('admin.JenisMakanan.index');
+    }
+}
---
+++ resources/views/admin/jenisMakanan/index.blade.php
@@ -0,0 +1,63 @@
+@extends('admin.layouts')
+
+@section('content-admin')
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Data Menu Makanan</h1>
+            <a href="{{ route('admin.makanan.create') }}"
+                class="d-none d-sm-inline-block btn btn-sm btn-info shadow-sm"><i
+                    class="fas fa-plus fa-sm text-white-50"></i> Tambah Data Menu Makanan</a>
+        </div>
+
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-info">Data Menu Makanan</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="tableMakanan" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Jenis Makanan</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>Jenis Makanan</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+
+                            @foreach ($JenisMakanan as $jm)
+                            <tr>
+                                <td>{{ $jm->JenisMakanan }}</td>
+                                <td>
+                                    <a href="{{ route('admin.JenisMakanan.edit', $jm) }}" class="btn btn-primary">Edit</a>
+                                    <a href="{{ route('admin.JenisMakanan.delete', $jm->id) }}"
+                                        class="btn btn-danger">Delete</a>
+                                </td>
+                            </tr>
+                            @endforeach
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+@endsection
+
+@push('script')
+
+    <script>
+        $(document).ready(function() {
+            $('#tableMakanan').DataTable();
+        });
+
+    </script>
+
+@endpush
