3be31bfd032a262cf0303a06b2006801e3e56305
Randy1927 <rizalrandy.saputra94@gmail.com>
2021-06-16 17:47:45 +0700

    push

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -30,8 +30,9 @@ class HomeController extends Controller
     {
         $this->middleware('role:admin');
 
-        $kamar = Kamar::all();
-        $kamarCount = $kamar->count();
+        $kamar = Kamar::sum('jumlah');
+        // dd($kamar);
+        // $kamarCount = $kamar;
 
         $cekIn = CekInCustomer::all();
         $cekInTrash = CekInCustomer::onlyTrashed()->get();
@@ -44,7 +45,7 @@ class HomeController extends Controller
         $makanan = MenuMakanan::all();
         $makananCount = $makanan->count();
 
-        return view('admin.dashboard', compact('kamarCount', 'cekInCount', 'customerCount', 'cekInDash', 'makananCount', 'cekInTrash'));
+        return view('admin.dashboard', compact('kamar', 'cekInCount', 'customerCount', 'cekInDash', 'makananCount', 'cekInTrash'));
     }
 
     public function hrd(){
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -20,7 +20,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Kamar Tersedia</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $kamarCount }}</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $kamar }}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-bed fa-2x text-gray-300"></i>
@@ -37,7 +37,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Kamar Terbooking</div>
+                                On Books</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $cekInCount }}</div>
                         </div>
                         <div class="col-auto">
