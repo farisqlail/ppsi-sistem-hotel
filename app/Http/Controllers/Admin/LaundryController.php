8e33efde96b0a685e4f591c316230a93e79894d5
farisqlail <faris.riskilail@gmail.com>
2021-06-16 18:30:43 +0700

    dashboard home

--- app/Http/Controllers/Admin/LaundryController.php
+++ app/Http/Controllers/Admin/LaundryController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use App\Models\Laundry;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
