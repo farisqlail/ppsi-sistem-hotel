be79ec784442f7f10bf830abfadbf8c205cb5523
Randy1927 <rizalrandy.saputra94@gmail.com>
2021-06-18 16:38:45 +0700

    pull

--- app/Http/Controllers/Admin/HistoryController.php
+++ app/Http/Controllers/Admin/HistoryController.php
@@ -1,7 +1,48 @@
 <?php
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Kamar;
+use App\Models\Customer;
+use App\Models\CekInCustomer;
+use App\Models\MenuMakanan;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 class HistoryController extends Controller
 {
+public function index()
+    {
+        $this->middleware('role:admin');
 
+        $kamar = Kamar::all();
+        // dd($kamar);
+        $kamarCount = $kamar->count();
+
+        $cekIn = CekInCustomer::all();
+        $cekInTrash = CekInCustomer::onlyTrashed()->get();
+        $cekInDash = CekInCustomer::orderBy('id', 'desc')->get();
+        $cekInCount = $cekIn->count();
+
+        $customer = Customer::all();
+        $customerCount = $customer->count();
+
+        $makanan = MenuMakanan::all();
+        $makananCount = $makanan->count();
+
+        return view('admin.History.History', compact('kamarCount', 'cekInCount', 'customerCount', 'cekInDash', 'makananCount', 'cekInTrash'));
+    }
+
+    public function hrd(){
+        $this->middleware('role:hrd');
+        
+        return view('hrd.dashboard');
+    }
+
+    public function karyawan(){
+        $this->middleware('role:karyawan');
+
+        return view('karyawan.dashboard');
+    }
 }
+
+
--- resources/views/admin/History/History.blade.php
+++ resources/views/admin/History/History.blade.php
@@ -1,113 +1,150 @@
 @extends('admin.layouts')
 
 @section('content-admin')
+<div class="container-fluid">
 
-     <div class="container-fluid">
-        <h1 class="h3 mb-2 text-gray-800">Cek In Customer</h1>
-
-        <div class="card shadow mb-4 mt-3">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Cek In Customer</h6>
-            </div>
-            <div class="card-body">
-
-                <form action="{{ route('admin.cekIn.update', $cekIn) }}" method="post">
-                    {{ csrf_field() }}
-                    {{ method_field('PATCH') }}
-
-                    <div class="row">
-                        <div class="col-md-6">                            
-                            <div class="form-group"> 
-                                <label for="">Nama Customer</label>
-                                <input type="text" name="namaCustomer" class="form-control" value="{{ $cekIn->namaCustomer }}">
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group"> 
-                                <label for="">Karyawan yang menangani</label>
-                                <input type="text" name="namaKaryawan" class="form-control" value="{{ $cekIn->namaKaryawan }}">
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group"> 
-                                <label for="">Jumlah Tamu</label>
-                                <select class="form-control" name="jumlahTamu">
-                                    <option selected>Pilih Jumlah Tamu</option>
-                                    <option value="1">1 Orang</option>
-                                    <option value="2">2 Orang</option>
-                                    <option value="3">3 Orang</option>
-                                  </select>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group"> 
-                                <label for="">Tanggal Cek In </label>
-                                <input type="text" name="tanggalCekIn" class="form-control" value="{{ date('d-m-Y') }}" readonly>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group"> 
-                                <label for="">Tanggal Cek Out</label>
-                                <input type="date" name="tanggalCekOut" class="form-control" value="{{ $cekIn->tanggalCekOut }}">
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group"> 
-                                <label for="">Jumlah Deposit (Rp)</label>
-                                <input type="text" name="deposit" id="deposit" class="form-control" value="{{ $cekIn->deposit }}">
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <hr>
-
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group"> 
-                                <label for="">Tipe Kamar</label>
-                                <select class="form-control" name="type_id">
-                                    <option selected>Pilih Tipe Kamar</option>
-                                    @foreach ($typeKamar as $tKamar)
-                                        <option value="{{ $tKamar->id }}">{{ $tKamar->typeKamar }}</option>
-                                    @endforeach
-                                  </select>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <label for="">Nomor Kamar</label>
-                            <select class="form-control" name="noKamar">
-                                <option selected>Pilih No Kamar</option>
-                                    @foreach ($kamar as $kmr)
-                                        <option value="{{ $kmr->noKamar }}">{{ $kmr->noKamar }}</option>
-                                    @endforeach
-                              </select>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <button class="btn btn-primary float-right" value="save" type="submit">Tambah</button>
-                    </div>
-                </form>
-
-            </div>
-        </div>
-     
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">History   </h1>
     </div>
 
+
+      <!-- Content Row -->
+      <div class="row">
+
+        <!-- Earnings (Monthly) Card Example -->
+       
+
+    <div class="row">
+
+        <!-- Area Chart -->
+        <div class="col-xl-6 col-lg-6">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div
+                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Customer Cek In Hari Ini</h6>
+                    <div class="dropdown no-arrow">
+                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                            aria-labelledby="dropdownMenuLink">
+                            <div class="dropdown-header">Dropdown Header:</div>
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    <table class="table table-bordered" id="tableCekIn" width="100%" cellspacing="0">
+                        <thead>
+                            <tr class="bg-muted">
+                                <th>No Kamar</th>
+                                <th>Nama Customer</th>
+                                <th>Tanggal Cek In</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr class="bg-muted">
+                                <th>No Kamar</th>
+                                <th>Nama Customer</th>
+                                <th>Tanggal Cek In</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+
+                            @foreach ($cekInDash as $ck)
+                            <tr class="bg-success text-white">
+                                <td>{{ $ck->noKamar }}</td>
+                                <td>{{ $ck->namaCustomer }}</td>
+                                <td>{{ $ck->tanggalCekIn }}</td>
+
+                            </tr>
+                            @endforeach
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <!-- Pie Chart -->
+        <div class="col-xl-6 col-lg-6">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div
+                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Customer Cek Out Hari Ini</h6>
+                    <div class="dropdown no-arrow">
+                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                            aria-labelledby="dropdownMenuLink">
+                            <div class="dropdown-header">Dropdown Header:</div>
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    <table class="table table-bordered" id="tableCekOut" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>No Kamar</th>
+                                <th>Nama Customer</th>
+                                <th>Tanggal Cek In</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>No Kamar</th>
+                                <th>Nama Customer</th>
+                                <th>Tanggal Cek In</th>
+                                <th>Tanggal Cek Out</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+
+                            @foreach ($cekInTrash as $ck)
+                                @if ($ck->trashed())
+                                    <tr class="bg-danger text-white">
+                                        <td>{{ $ck->noKamar }}</td>
+                                        <td>{{ $ck->namaCustomer }}</td>
+                                        <td>{{ $ck->tanggalCekIn }}</td>
+        
+                                    </tr>
+                                @endif
+                            @endforeach
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+</div>
 @endsection
 
 @push('script')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"></script>
+
     <script>
-        $('#deposit').mask('000.000.000.000', {
-            reverse: true
+        $(document).ready(function() {
+            $('#tableCekIn').DataTable();
+            $('#tableCekOut').DataTable();
         });
 
     </script>
+
 @endpush
